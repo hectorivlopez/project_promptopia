c7e710f95b45ddfd6d25461a50b623c7241a76b2
hectorivlopez <131830107+hectorivlopez@users.noreply.github.com>
2023-06-28 02:13:40 -0600

    Seventh test

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -5,11 +5,13 @@ export const GET = async (req) => {
 	try {
 		await connectToDB();
 
-		const prompts = await Prompt.find().populate('creator');
+		const prompts = await Prompt.find({
+
+		}).populate('creator');
 
 		return new Response(JSON.stringify(prompts), { status: 200 });
 	}
 	catch (error) {
-		return new Response('Failed to fetch all prompts', { status: 500 });
+		return new Response('Failed to fetch user prompts', { status: 500 });
 	}
 }
--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -14,7 +14,7 @@ const UserProfile = ({params}) => {
 
 	useEffect(() => {
 		const fetchPosts = async () => {
-			const response = await fetch(`/api/prompt`);
+			const response = await fetch(`/api/users/${params.id}/posts`);
 			const data = await response.json();
 
 			setPosts(data);
