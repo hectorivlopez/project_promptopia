d91b6c137a12c63d9227daf0d417fdd54394d53b
hectorivlopez <131830107+hectorivlopez@users.noreply.github.com>
2023-06-28 00:22:46 -0600

    First commit

---
+++ app/profile/[id]/page.jsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { useState, useEffect } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import Profile from "@components/Profile";
+
+
+
+const UserProfile = ({params}) => {
+	const searchParams = useSearchParams();
+	const username = searchParams.get('user');
+	
+	const [posts, setPosts] = useState([]);
+
+	useEffect(() => {
+		const fetchPosts = async () => {
+			const response = await fetch(`/api/users/${params.id}/posts`);
+			const data = await response.json();
+
+			setPosts(data);
+		}
+
+		fetchPosts();
+	}, []);
+
+	return (
+		<Profile
+			name={username}
+			desc={`Welcome to ${username} profile page`}
+			data={posts}
+	
+		/>
+	)
+}
+
+export default UserProfile
---
+++ components/Feed.jsx
@@ -0,0 +1,118 @@
+'use client';
+
+import { useState, useEffect } from "react";
+
+import PromptCard from "./PromptCard";
+
+const PromptCardList = ({data, handleTagClick}) => {
+	return (
+		<div className="mt-16 prompt_layout">
+			{data.map(post => (
+				<PromptCard
+					key={post._id}
+					post={post}
+					handleTagClick={handleTagClick}
+				/>
+			))}
+		</div>
+	)
+}
+
+const Feed = () => {
+	const [searchText, setSearchText] = useState('');
+	const [posts, setPosts] = useState([]);
+	const [feedPosts, setFeedPosts] = useState([]);
+	const [usernames, setUsernames] = useState([]);
+	const [tags, setTags] = useState([]);
+
+	useEffect(() => {
+		const fetchPosts = async () => {
+			const response = await fetch('/api/prompt');
+			const data = await response.json();
+
+			setPosts(data);
+			setFeedPosts(data)
+		}
+
+		fetchPosts();
+	}, []);
+
+	useEffect(() => {
+		const getUsernames = () => {
+			let postsUsernames = []
+			for (let post of posts) {
+				if (!postsUsernames.includes(post.creator.username)) {
+					postsUsernames.push(post.creator.username);
+				}
+			}
+			setUsernames(postsUsernames);
+		}
+		getUsernames();
+
+		const getTags = () => {
+			let postsTags = []
+			for (let post of posts) {
+				if (!postsTags.includes(post.tag)) {
+					postsTags.push(post.tag);
+				}
+			}
+			setTags(postsTags);
+		}
+		getTags();
+	}, [posts])
+
+	const searchPosts = (target) => {
+		if (target === '') {
+			setFeedPosts(posts);
+			return
+		}
+
+		if (usernames.includes(target)) {
+			const filteredPosts = posts.filter(post => post.creator.username === target)
+			setFeedPosts(filteredPosts);
+			return
+		}
+
+		if (tags.includes(target)) {
+			const filteredPosts = posts.filter(post => post.tag === target)
+			setFeedPosts(filteredPosts);
+			return
+		}
+
+		const filteredPosts = posts.filter(post => post.prompt.includes(target));
+		setFeedPosts(filteredPosts)
+	}
+
+	const handleSearchChange = (e) => {
+		setSearchText(e.target.value);
+		searchPosts(e.target.value);
+	}
+
+	const handleTagClick = (tag) => {
+		setSearchText(tag);
+		searchPosts(tag)
+	}
+
+
+	return (
+		<section className="feed">
+			<form className="relative w-full flex-center">
+				<input
+					type="text"
+					placeholder="Search for a tag or username"
+					value={searchText}
+					onChange={handleSearchChange}
+					required
+					className="search_input peer"
+				/>
+			</form>
+
+			<PromptCardList
+				data={feedPosts}
+				handleTagClick={handleTagClick}
+			/> 
+		</section>
+	)
+}
+
+export default Feed
