e875488f380c5f36fcf908eaf041c8193a4530c2
hectorivlopez <131830107+hectorivlopez@users.noreply.github.com>
2023-06-28 03:39:02 -0600

    Nineth test

--- app/api/users/[id]/posts/route.js
+++ app/api/users/[id]/posts/route.js
@@ -6,7 +6,7 @@ export const GET = async (req, { params }) => {
 		await connectToDB();
 
 		const prompts = await Prompt.find({
-			creator: params.id
+
 		}).populate('creator');
 
 		return new Response(JSON.stringify(prompts), { status: 200 });
